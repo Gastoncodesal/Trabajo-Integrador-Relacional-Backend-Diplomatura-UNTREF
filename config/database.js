5b8c1c2d888969fc994f459379b034f0cbc875ee
Gastoncodesal <codesalgaston8@gmail.com>
2024-11-05 11:57:51 -0300

    config  agregado

--- config/database.js
+++ config/database.js
@@ -1,9 +1,17 @@
 const { Sequelize } = require('sequelize')
-process.loadEnvFile()
+const dotenv = require ("dotenv")
+const ENV = process.env.NODE_ENV || "local"
+dotenv.config({ path: `.env.%{ENV}`})
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
-  dialect: process.env.DB_DIALECT
+const sequelize = new Sequelize 
+( 
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_USER_PASSWORD,
+  {host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+ dialect: process.env.DB_DIALECT,
+  
 })
 
 module.exports = sequelize
