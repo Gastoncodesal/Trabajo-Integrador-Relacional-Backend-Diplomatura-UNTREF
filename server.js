75fa2bd982fbca74f55d194eb7f1351f39f701a4
Gastoncodesal <codesalgaston8@gmail.com>
2024-11-05 17:37:27 -0300

    server

--- server.js
+++ server.js
@@ -5,23 +5,26 @@ const RTactores = require('./routes/actores.routes')
 const RTid = require('./routes/id.routes')
 const RTfiltrar = require('./routes/filtrar.routes')
 const bodyParser = require('body-parser')
-
+const { swaggerUi, swaggerDocs } = require('./swagger config/swagger.confg')
 
 const app = express()
 const port = 3000
 
-
 app.get('/', (req, res) => {
-  res.send('Hola mundo !')
+  res.send('¡Hola mundo!')
 })
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 app.use(bodyParser.json())
 app.use('', RTcontenido)
 app.use('', RTfiltrar)
 app.use('', RTactores)
 app.use('', RTid)
 
+
+
 app.listen(port, async () => {
   await sequelize.authenticate()
-  console.log(`server funcando en http://localhost:${port}`)
+  console.log(`server funcionando en http://localhost:${port}`)
+  console.log(`Docuentación de la API en http://localhost:${port}/api-docs`)
 })
